e66d7e063be0cb28ed174da1b6c806eeb0394316
mihir20 <mihir20121997@gmail.com>
2018-07-11 01:35:56 +0530

    check if que is available or not

--- killerScript.js
+++ killerScript.js
@@ -1,12 +1,13 @@
-console.log("welcome");
-console.log(location.href);
 const qustionUrl = "https://www.chegg.com/homework-help/expertquestion";
 const captchaUrl = "https://www.chegg.com/recaptcha?recaptcha_redirect_url=https://www.chegg.com/homework-help/expertquestion&invalid_captcha_return_url=https://www.chegg.com/my/expertqa";
+const noQuesText = "Unfortunately, there are no questions available in your queue for the moment"
 
 if (location.href == qustionUrl) {
-    console.log("Check if there is a question or not");
-    chrome.runtime.sendMessage({ redirect: qustionUrl, load: true });
+    if(document.body.innerText.search(noQuesText)>0){
+        chrome.runtime.sendMessage({ redirect: qustionUrl, load: true });
+    }else{
+        console.log("kaam ho raha hai")
+    }
 }else if (location.href == captchaUrl){
-    console.log("redirect to qustion url")
     chrome.runtime.sendMessage({ redirect: qustionUrl , load: true});
 }
