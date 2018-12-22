ae40fb85fc09b18c78d9b17e93879ec0ba111736
mihir20 <mihir20121997@gmail.com>
2018-12-22 11:35:42 +0530

    updated urls

--- killerScript.js
+++ killerScript.js
@@ -1,7 +1,8 @@
 const qustionUrl = "https://www.chegg.com/homework-help/expertquestion";
 const captchaUrl = "https://www.chegg.com/recaptcha?recaptcha_redirect_url=https://www.chegg.com/homework-help/expertquestion&invalid_captcha_return_url=https://www.chegg.com/my/expertqa";
-const noQuesText = "Unfortunately, there are no questions available in your queue for the moment"
-
+const noQuesText = "Unfortunately, there are no questions available in your queue for the moment";
+const homeUrl = "https://www.chegg.com";
+const otherCaptchaUrl = "https://www.chegg.com/recaptcha?recaptcha_redirect_url=https://prod.cheggstudy.prod2.cheggnet.com/homework-help/expertquestion&invalid_captcha_return_url=https://www.chegg.com/my/expertqa";
 if (location.href == qustionUrl) {
     if(document.body.innerText.search(noQuesText)>0){
         chrome.runtime.sendMessage({ redirect: qustionUrl, load: true });
@@ -10,4 +11,11 @@ if (location.href == qustionUrl) {
     }
 }else if (location.href == captchaUrl){
     chrome.runtime.sendMessage({ redirect: qustionUrl , load: true});
+}else if (location.href == otherCaptchaUrl){
+    chrome.runtime.sendMessage({ redirect: qustionUrl , load: true});
+}
+else if (location.href == homeUrl){
+    chrome.runtime.sendMessage({redirect: qustionUrl , load:true})
+}else{
+    chrome.runtime.sendMessage({redirect: qustionUrl, load:true})
 }
