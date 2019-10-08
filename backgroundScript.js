4fc08ba5bdd466a2fb71139c1b372e5df5e32294
Mihir Gandhi <mihir20121997@gmail.com>
2019-10-08 14:01:24 +0530

    Update backgroundScript.js

--- backgroundScript.js
+++ backgroundScript.js
@@ -1,10 +1,10 @@
 
-chrome.runtime.onMessage.addListener(function (request, sender) {
-    if (request.load){
-        chrome.tabs.update(sender.tab.id, { url: request.redirect });
+chrome.runtime.onMessage.addListener(function (req, sender) {
+    if (req.load){
+        chrome.tabs.update(sender.tab.id, { url: req.redirect });
     }else{
         var audio = new Audio();
-        audio.src = "./notification_sound.mp3"
+        audio.src = "./notification_sound.mp3";
         audio.play();
     }
 });
